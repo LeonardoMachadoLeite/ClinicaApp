46aaaf5a31bcdefd7acc3852c87d3b8501e5bf12
Leonardo Machado Leite <leomleite@hotmail.com>
2020-05-25 19:05:41 -0300

    Adição de controladores de Pagamento e FormaPagamento

--- app/src/main/java/com/milkaxe_studios/clinicaapp/controllers/FormaPagamentoController.java
+++ app/src/main/java/com/milkaxe_studios/clinicaapp/controllers/FormaPagamentoController.java
@@ -1,4 +1,93 @@
 package com.milkaxe_studios.clinicaapp.controllers;
 
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.milkaxe_studios.clinicaapp.model.ActivityController;
+import com.milkaxe_studios.clinicaapp.model.FormaPagamento;
+
 public class FormaPagamentoController {
+
+    private ActivityController activity;
+    private DatabaseReference mDatabase;
+    private SharedPreferences preferences;
+
+    public FormaPagamentoController(ActivityController activity, SharedPreferences preferences) {
+        this.activity = activity;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        this.preferences = preferences;
+    }
+
+    public void inserirFormaPagamento(FormaPagamento formaPagamento) {
+        activity.setProgressBarVisible();
+
+        DatabaseReference espReference = mDatabase.child("FormaPagamento").push();
+        formaPagamento.Id = espReference.getKey();
+
+        espReference.setValue(formaPagamento)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        System.out.println("Forma de Pagamento Cadastrado!");
+                        activity.notifyActivity("Finish");
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        activity.setProgressBarInvisible();
+                        activity.toast("Falha no Cadastro da Forma de Pagamento");
+                    }
+                });
+    }
+
+    public void getFormaPagamento(final String id, final String...tags) {
+        activity.setProgressBarVisible();
+
+        mDatabase.child("FormaPagamento").child(id)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        FormaPagamento formaPagamento = dataSnapshot.getValue(FormaPagamento.class);
+                        preferences.edit().putString("FormaPagamento/Get", formaPagamento.toString()).apply();
+                        activity.notifyActivity(tags);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    public void atualizarEndereco(FormaPagamento formaPagamento) {
+        activity.setProgressBarVisible();
+
+        DatabaseReference espReference = mDatabase.child("FormaPagamento").child(formaPagamento.Id);
+
+        espReference.setValue(formaPagamento)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        activity.toast("Forma de Pagamento Atualizado!");
+                        activity.finish();
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        activity.setProgressBarInvisible();
+                        activity.toast("Falha na Atualização da Forma de Pagamento");
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/milkaxe_studios/clinicaapp/controllers/PagamentoController.java
+++ app/src/main/java/com/milkaxe_studios/clinicaapp/controllers/PagamentoController.java
@@ -1,4 +1,93 @@
 package com.milkaxe_studios.clinicaapp.controllers;
 
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.milkaxe_studios.clinicaapp.model.ActivityController;
+import com.milkaxe_studios.clinicaapp.model.Pagamento;
+
 public class PagamentoController {
+
+    private ActivityController activity;
+    private DatabaseReference mDatabase;
+    private SharedPreferences preferences;
+
+    public PagamentoController(ActivityController activity, SharedPreferences preferences) {
+        this.activity = activity;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        this.preferences = preferences;
+    }
+
+    public void inserirPagamento(Pagamento pagamento) {
+        activity.setProgressBarVisible();
+
+        DatabaseReference espReference = mDatabase.child("Pagamento").push();
+        pagamento.Id = espReference.getKey();
+
+        espReference.setValue(pagamento)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        System.out.println("Pagamento Cadastrado!");
+                        activity.notifyActivity("Finish");
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        activity.setProgressBarInvisible();
+                        activity.toast("Falha no Cadastro de Pagamento");
+                    }
+                });
+    }
+
+    public void getPagamento(final String id, final String...tags) {
+        activity.setProgressBarVisible();
+
+        mDatabase.child("Pagamento").child(id)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Pagamento pagamento = dataSnapshot.getValue(Pagamento.class);
+                        preferences.edit().putString("Pagamento/Get", pagamento.toString()).apply();
+                        activity.notifyActivity(tags);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    public void atualizarPagamento(Pagamento pagamento) {
+        activity.setProgressBarVisible();
+
+        DatabaseReference espReference = mDatabase.child("Pagamento").child(pagamento.Id);
+
+        espReference.setValue(pagamento)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        activity.toast("Pagamento Atualizado!");
+                        activity.finish();
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        activity.setProgressBarInvisible();
+                        activity.toast("Falha na Atualização no Pagamento");
+                    }
+                });
+    }
+
 }
